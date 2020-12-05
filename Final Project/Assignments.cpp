23d04471fb04ba3223d44ed01bc4a88918e9df9f
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2020-12-04 23:57:52 -0500

    Update and rename AssignmentDueDates.cpp to Assignments.cpp

---
+++ Final Project/Assignments.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <list>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class AssignmentDueDates
+{
+public:
+
+//Assignment Title
+string title;
+int day, month = 0;
+
+//Due Date set to Default
+double dueDate = 1.01;
+
+//Pointer Variable
+    int *ptr;
+
+   
+private:
+
+
+
+   
+
+    //Iterator
+    map<string, double>::iterator it;
+
+    
+
+};
